f9fc8b0ff4730020bb2ce9b86095ad70f8c4b294
Carlos H. Marian <carlosmarian@gmail.com>
2018-07-31 21:33:23 -0300

    first component

---
+++ src/HelloWorld.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class HelloWorld extends Component{
+
+    //Metodo para mostrar ocomponente
+    render(){
+        return (
+            <div className="my-class-from-react">
+                <h1>Olá mundo com ReactJS</h1>
+            </div>
+        )        
+    }
+
+}
+
+export default HelloWorld
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import HelloWorld from './HelloWorld';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('root')
+)
